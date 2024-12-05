0255e6d06aa467c4dea14a9decff2e7ac7e31535
Alexandros-OD <alexandros.kakakis@onlinedialogue.com>
2024-12-05 09:16:22 +0100

    WIP day 5

--- src/day_05.py
+++ src/day_05.py
@@ -26,12 +26,25 @@ incorrect_updates = [
 
 """
 TODO: replace each incorrect occurance with the correct rule
-- Iterate over rules: create regex, create replacement string, find index of rule violation, replace, update rule
+- Iterate over rules:
+    - replace first number
+    - delete second number
+    - update update
 - Check if updated rules pass rule_regex
 - Get middle number of updated rules
 """
 
+incorrect_update = "61,13,29"
+
 for rule in rules:
-    single_rule_regex = ""
-    print(rule)
-    break
+    first_number, second_number = rule.split('|')
+    
+    single_rule_regex = f"{second_number},.*{first_number}"
+    replacement_string = f"{first_number},{second_number}"
+    
+    start_index = re.search(single_rule_regex, incorrect_update).start() if re.search(single_rule_regex, incorrect_update) else -1
+    if start_index != -1:
+        print(
+            single_rule_regex,
+            incorrect_update[start_index:start_index+2]
+        )
