4dc62230bc2aaa12694dcf18c58d80d947c1d307
Alexandros Kakakis <alexandroskakakis@gmail.com>
2024-12-11 17:22:01 +0100

    Add solution day 11

--- src/day_11.py
+++ src/day_11.py
@@ -1,34 +1,48 @@
+from collections import defaultdict
+
 with open("data/puzzle_input_day_11.txt", "r") as file:
     stone_arrangement = [ int(i) for i in file.read().split(" ") ]
 
-
-def update_stone_arrangement(stone_arrangement):
-    next_stone_arrangement = []
-
-    for stone in stone_arrangement:
-        digits = len(str(stone)) 
+def process_stone(stone):
+    digits = len(str(stone))
+    if stone == 0:
+        return [1]
         
-        if stone == 0:
-            next_stone_arrangement.append(1) ## hier hoef ik alleen te weten in welke iteratie 1 is toegevoegd, en dus niet meenemen in de next_stone_arrangement
-            
-        elif digits % 2 == 0:
-            divisor = 10 ** (digits // 2)
-            next_stone_arrangement += [
-                stone // divisor,
-                stone % divisor
-            ]
-            
-        else:
-            next_stone_arrangement.append(stone * 2024)
-    
-    return next_stone_arrangement
+    elif digits % 2 == 0:
+        divisor = 10 ** (digits // 2)
+        return [
+            stone // divisor,
+            stone % divisor
+        ]
+        
+    else:
+        return [stone * 2024]
 
-for _ in range(75):
-    print(_)
-    # print(stone_arrangement)
-    stone_arrangement = update_stone_arrangement(stone_arrangement)
-    
+
+def blink(stone_dict):
+    new_stone_dict = defaultdict(int)
+    for stone, count in stone_dict.items():
+        new_stones = process_stone(stone)
+        for new_stone in new_stones:
+            new_stone_dict[new_stone] += count
+
+    return new_stone_dict
+
+stone_dict = defaultdict(int)
+
+for stone in stone_arrangement:
+    stone_dict[int(stone)] += 1
+
+for i in range(75):
+    stone_dict = blink(stone_dict)
+
+    if i == 24:
+        print(
+            "Solution Part One:",
+            sum(stone_dict.values())
+        )
+
 print(
-    "Solution Part One:",
-    len(stone_arrangement)
+    "Solution Part Two:",
+    sum(stone_dict.values())
 )
